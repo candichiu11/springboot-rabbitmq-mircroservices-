2ac39b357f1cd37ae092f4cfe3654fb3c160e512
Candi Lilun Chiu <candi.chiu@egym.com>
2024-01-04 15:31:20 +0100

    implement order service using rabbitmq

---
+++ src/test/java/com/candicode/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candicode.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
